ad3566b2f42c3a2d513bc5c2977750565aed6c07
Pillar <pillar_x@163.com>
2026-02-15 16:04:55 +0800

    Month1 finished

--- week4/model.py
+++ week4/model.py
@@ -1,7 +1,9 @@
 import os
+import inspect
 from typing import Optional, Union, Self
 
 from sympy import transpose
+from torch.optim import AdamW
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"  # 允许重复加载 OpenMP
 os.environ["OMP_NUM_THREADS"] = "1"          # 可选：限制线程数，避免过多线程
@@ -57,7 +59,6 @@ data = torch.tensor(encode(text),dtype=torch.long).to(device)
 
 
 class MLP(nn.Module):
-
     def __init__(self, config):
         super().__init__()
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
@@ -110,13 +111,24 @@ class CausalSelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         return y
 
+    def config_optimizer(self,weight_decay,learning_rate,betas,device_type):
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn : p for pn , p in param_dict if p.requires_grad}
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
 
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0},
+        ]
+        #num_decay_params = sum(p.numel() for p in decay_params)
+        #num_nodecay_params = sum( p.numel() for p in nodecay_params)
 
-
-
-
-
-
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == 'cuda'
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = AdamW(optim_groups,lr = learning_rate,betas = betas,**extra_args)
+        return optimizer
 
 
 class Block(nn.Module):
@@ -163,8 +175,6 @@ class MiniGPT(nn.Module):
             n_params -= self.transformer.wte.weight.numel()
         return n_params
 
-
-
     def _init_weights(self,module):
         if isinstance(module,nn.Linear):
             torch.nn.init.normal_(module.weight,mean=0,std=0.02)
@@ -173,57 +183,40 @@ class MiniGPT(nn.Module):
         elif isinstance(module,nn.Embedding):
             torch.nn.init.normal_(module.weight,mean=0,std=0.02)
 
+    def forward(self,idx,targets = None):
+        B,T = idx.size()
+        assert T <= self.config.block_size
+        pos = torch.arange(0,T,dtype=torch.long,device=idx.device)
 
-    #以下还未修改-----------------------------------------------------------------------------
-    def forward(self,x,target):
-        B,T = x.shape
-        x = self.token_embedding_table(x)
-        pos = self.position_embedding_table(torch.arange(T,device=x.device))
-        x = x + pos
+        tok_emb = self.transformer.wte(idx)
+        pos_emb = self.transformer.wpe(pos)
 
-        x = self.blocks(x)
-        x = self.ln_f(x)
-        logits = self.lm_head(x)
-        loss = None
+        x = self.transformer.dropout(tok_emb+pos_emb)
 
-        if target is not None:
-            B,T,C = logits.shape
-            logits_flat = logits.view(B*T,C)
-            target_flat = target.view(B*T)
-            loss = F.cross_entropy(logits_flat, target_flat)
+        for block in self.transformer.blocks:
+            x = block(x)
+        x = self.transformer.ln_f(x)
 
+        if targets is not None:
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(logits.view(-1,logits.size(-1)),targets.view(-1),ignore_index=-1)
+        else:
+            logits = self.lm_head(x[:,[-1],:])
+            loss = None
         return logits,loss
 
-    def generate(self,idx,max_token_size):
 
-        for _ in range(max_token_size):
-            idx_cond = idx[:,-block_size:]
-            logits,_ = self.forward(idx_cond,None)
-            logits_last = logits[:,-1,:]
-            probs = F.softmax(logits_last,dim=-1)
+
+    def generate(self,idx,max_new_tokens,temperature = 1.0, top_k=None):
+        for _ in range(max_new_tokens):
+            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:,-self.config.block_size:]
+            logits, _ = self(idx_cond)
+            logits = logits[:,-1,:]/temperature
+            if top_k is not None:
+                v,_ = torch.topk(logits,min(top_k,logits.size(-1)))
+                logits[logits < v[:,[-1]]] = -float('inf')
+            probs = F.softmax(logits,dim=-1)
             idx_next = torch.multinomial(probs,1)
             idx = torch.cat([idx,idx_next],dim=1)
+
         return idx
-
-m = MiniGPT(vocab_size).to(device)
-optimizer = torch.optim.AdamW(m.parameters(),lr=learning_rate)
-
-max_steps = 300
-
-loss_figure = []
-
-
-for step in range(max_steps):
-    x,y = get_batch(data,device)
-    logits,loss = m(x,y)
-    loss_figure.append(loss.item())
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    if(step % 10 == 0):
-        print(f"step: {step}, loss: {loss.item()}")
-
-start_idx = torch.zeros((1, 1), dtype=torch.long, device=device)
-max_new_tokens = 300
-sampled = m.generate(start_idx, max_new_tokens)[0].tolist()
-print(''.join(decode(sampled)))
